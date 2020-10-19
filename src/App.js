bf2887cec14acb36d884de737687f33a8dd83df2
bp40 <60870525+bp40@users.noreply.github.com>
2020-10-19 09:50:56 +0700

    changed App class to function; randomOrderHandler is now a function; (Intro to hooks)

--- src/App.js
+++ src/App.js
@@ -1,40 +1,36 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-
-  state = {
+const App = props => {
+  const [ personState, setPersonState ] = useState({
     persons: [
       {name: 'test', color: 'blue'},
       {name: 'wooooo', color: 'green'},
     ]
-  }
+  });
 
-  randomOrderHandler = () => {
+  const randomOrderHandler = () => {
     //console.log("Button was clicked");
-    this.setState({
+    setPersonState({
       persons: [
         {name: 'IT CHANGED!!!', color: 'red'},
         {name: 'wooooo', color: 'green'},
       ]
     })
   }
-
-  render() {
-    return(
+  
+  return(
       <div className="App">
         <h1>This is the most awesome home page ever!</h1>
         <p>this is a paragraph tag</p>
-        <button onClick={this.randomOrderHandler}>RANDOMIZER3000</button>
-        <Person name={this.state.persons[0].name} color={this.state.persons[0].color}>THE CHILDRENS</Person>
-        <Person name={this.state.persons[1].name} color={this.state.persons[1].color}>Hope this works</Person>
+        <button onClick={randomOrderHandler}>RANDOMIZER3000</button>
+        <Person name={personState.persons[0].name} color={personState.persons[0].color}>THE CHILDRENS</Person>
+        <Person name={personState.persons[1].name} color={personState.persons[1].color}>Hope this works</Person>
       </div>
     );
   } 
   //same as
   //return React.createElement('div', null, React.createElement('h1', null, 'This is using createElement'))
-}
-
 
 export default App;

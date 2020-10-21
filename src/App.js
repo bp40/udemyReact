bb1603d9721be9cb5fc6b7676ac6f9f7b7273c8b
bp40 <60870525+bp40@users.noreply.github.com>
2020-10-21 15:23:01 +0700

    edited spacing

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-  import React, { Component, useState } from 'react';
-  import './App.css';
-  import Person from './Person/Person';
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
 
-  class App extends Component {
-    state = {
-      nameColor: [
-        {name: 'test', color: 'blue'},
-        {name: 'wooooo', color: 'green'},
-      ],
+class App extends Component {
+  state = {
+    nameColor: [
+      {name: 'test', color: 'blue'},
+      {name: 'wooooo', color: 'green'},
+    ],
       otherState: "if you have one"
     };
 
-    randomOrderHandler = () => {
-      this.setState({
-        nameColor: [
-          {name: 'tset', color: 'orange'},
-          {name: 'ooooow', color: 'red'},
-        ]
-      });
-    }
-    render () {
-      return(
-        <div className="App">
-          <h1>This is the most awesome home page ever!</h1>
-          <p>this is a paragraph tag</p>
-          <button onClick={this.randomOrderHandler}>RANDOMIZER3000</button>
-          <Person name={this.state.nameColor[0].name} color={this.state.nameColor[0].color}>THE CHILDRENS</Person>
-          <Person name={this.state.nameColor[1].name} color={this.state.nameColor[1].color}>Hope this works</Person>
-        </div>
+  randomOrderHandler = () => {
+    this.setState({
+      nameColor: [
+        {name: 'tset', color: 'orange'},
+        {name: 'ooooow', color: 'red'},
+      ]
+    });
+  }
+  render () {
+    return(
+      <div className="App">
+        <h1>This is the most awesome home page ever!</h1>
+        <p>this is a paragraph tag</p>
+        <button onClick={this.randomOrderHandler}>RANDOMIZER3000</button>
+        <Person name={this.state.nameColor[0].name} color={this.state.nameColor[0].color}>THE CHILDRENS</Person>
+        <Person name={this.state.nameColor[1].name} color={this.state.nameColor[1].color}>Hope this works</Person>
+      </div>
         );
       } 
   //same as
